d29a41cb15adf8d280888c6f196c6af149f0c26d
Viktoriya May <119529675+Viktoriya-May@users.noreply.github.com>
2023-05-05 21:08:27 +0200

    upd fom

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
 
+import throttle from 'lodash.throttle';
+
+
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('.feedback-form input');
+const textInput = document.querySelector('.feedback-form textarea');
+
+const LOCAL_KEY = 'feedback-form-state';
+const formData = {};
+
+getDataFromStorage();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+function onFormInput(event) {
+    const formData = form.elements;
+
+    const email = formData.email.value;
+    const message = formData.message.value;
+    const dataToSave = { email, message };
+
+    localStorage.setItem( LOCAL_KEY, JSON.stringify(dataToSave));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const {elements: { email, message }}  = event.currentTarget;
+
+    if (email.value === "" || message.value === "") {
+        return window.alert("Усі поля повинні бути заповнені!");
+    } 
+
+    console.log(JSON.parse(localStorage.getItem( LOCAL_KEY)));
+    
+    event.currentTarget.reset();
+    localStorage.removeItem(LOCAL_KEY);
+}
+
+function getDataFromStorage() {
+    const savedData = JSON.parse(localStorage.getItem(LOCAL_KEY));
+
+    if (savedData) {
+        emailInput.value = savedData.email;
+        textInput.value = savedData.message;
+    }
+}
